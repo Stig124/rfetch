086d59d24d95fd7210dd5d46b14abd6bc59e73de
Nicolas Stig124 FORMICHELLA <stigpro@outlook.fr>
2021-03-20 13:20:51 +0100

    Multiple modifications

---
+++ README.md
@@ -0,0 +1,140 @@
+  <!-- <a href="https://rfetch.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">Rfetch</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/stig124/rfetch?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/stig124/rfetch?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/stig124/rfetch?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/stig124/rfetch?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/stig124/rfetch?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/stig124/rfetch?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/stig124/rfetch?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center">
+	🚧  Rfetch 🚀 Under construction...  🚧
+</h4>
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0;
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/stig124" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+Fetch and applies the most upvoted image in any image subreddit as a wallpaper (Windows only for the moment)
+
+## :sparkles: Features ##
+
+:heavy_check_mark: Automatic fetching and validation of grabbed URLs
+:heavy_check_mark: Choosing a random post (coming soon)
+:heavy_check_mark: Automatic prerequirements install script (coming soon)
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- [PRAW](https://github.com/adrn/pyraw)
+- [Reddit API](https://www.reddit.com/dev/api)
+- [Google Python-Fire](https://github.com/google/python-fire)
+- You can find the python modules list in [requirements.txt](./requirements.txt)
+
+## :white_check_mark: Requirements ##
+
+Before starting :checkered_flag:, you need to have
+
+- [Git](https://git-scm.com)
+- [Python 3](https://python.org)
+- [ImageMagick](https://imagemagick.org/script/download.php)
+- a Reddit app registered as a `script`, see [Reddit API Quick Start](https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example#first-steps), use a dummy redirect URL like `http://localhost:8080`
+
+## :checkered_flag: Starting ##
+
+##### Clone this project #####
+
+```shell
+git clone https://github.com/stig124/rfetch
+```
+
+##### Access the directory #####
+
+```shell
+cd rfetch
+```
+
+##### Grab the required python packages *(NOTE : a virtualenv is highly recommanded to avoid pakcage mess)* #####
+
+```shell
+pip install -r requirements.txt
+```
+
+##### Modifiy the [settings.yaml.sample](./settings.yaml.sample) file with the corresponding settings then #####
+
+```shell
+cp settings.yaml.sample settings.yaml
+```
+
+##### RUN #####
+
+```shell
+python ./main.py
+```
+
+and let the magic happen :sparkles:
+
+__NOTE__ : **There is no way to revert a wallpaper change !** *(an history of images link may come later)*
+
+## Usage ##
+
+Running `python ./main.py` fetches all the settings from the `settings.yaml` files but is is possible to change those settings on the fly :
+
+```shell
+python ./main.py <SUBNAME> <DELETE>
+```
+
+`Subname` :
+Overrides the subreddit name defined in the config file [Syntax : subreddit name wihout `r/`]
+
+`Delete` :
+Opting in the automatic deletion of the image ater being set as a wallpaper [Syntax : `True` or `False`]
+
+##### :bangbang: Warning
+
+Some subreddits will not work if they don't use URLs with image type ending (xxx.xxx.jpg/png)
+Tested and working subs :
+
+- r/earthporn
+- r/villageporn
+- r/cityporn
+- r/wallpaper
+- r/Animewallpaper
+- r/ComicWalls
+
+## :memo: License ##
+
+This project is under Mozilla Public License 2.0. For more details, see the [LICENSE](./LICENSE) file.
+
+Made with :heart: by <a href="https://github.com/stig124" target="_blank">Stig124</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- main.py
+++ main.py
@@ -1,29 +1,79 @@
-import yaml
-from wand.image import Image
-import requests as curl
-import praw
-import validators
 import ctypes
 import os
+import fire
+import praw
+import requests as curl
+import validators
+import yaml
+from wand.image import Image
 
 conf = yaml.safe_load(open('settings.yaml'))
 cid = str(conf['app']['client_id'])
 secret = str(conf['app']['client_secret'])
 user = str(conf['user']['rname'])
-uagent = "python:rfetch:v0.0.1a (by " + user
+uagent = "python:rfetch:v0.0.3b (by " + user
+delete = bool(conf['user']['delete'])
 
-reddit = praw.Reddit(client_id=cid, client_secret=secret, user_agent=uagent, )
-subname = str(conf['user']['subname'])
-subreddit = reddit.subreddit(subname)
+reddit = praw.Reddit(client_id=cid, client_secret=secret, user_agent=uagent, )  # Declare the Reddit instance
+subname = str(conf['user']['subname'])  # Get the requested subreddit from the config file
+subreddit = reddit.subreddit(subname)  # Declare the requested subreddit
+
+
+def prereq():
+    with os.system('cmd /c "magceick"') as cmd:
+        if cmd.returncode != 0:
+            print("ImageMagick is not installed on your system (or your path is mismatched)")
+            print("If you want, I can install it automatically using chocolatey")
+            print("This will install ImageMagick, gsudo and Chocolatey (if not installed)")
+            ins = input("Install? y or n")
+            if ins == "y" or ins == "n":
+                with os.system('cmd /c "choco"') as cmd2:
+                    if cmd2.returncode != 0:
+                        print("Installing choco")
+                        choco()
+                    else:
+                        magick()
+            else:
+                print("Wrong Input, exiting")
+
+
+def choco():
+    with os.system('PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"') as gsudoins:
+        if gsudoins.returncode != 0:
+            print("gsudo installed failed")
+        else:
+            with os.system('gsudo Powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))"') as chocoins:
+                if chocoins.returncode != 0:
+                    print("choco install failed")
+                else:
+                    with os.system('cmd /c "choco"') as ctest:
+                        if ctest.returncode != 0:
+                            print("choco install failed")
+                        else:
+                            print("Chocolatey install succeded, installing ImageMagick")
+
+
+def conf(clisubname=subname, clidelete=delete):
+    """
+    Alter the .yaml configured settings (if nothing given, the defaults are in the yaml file)
+    :param clisubname: Name of the subreddit, without the r/
+    :param clidelete: If you want to delete the file after the wallpaper is changed enter True
+    """
+    global subname
+    subname = clisubname
+    global subreddit
+    subreddit = reddit.subreddit(clisubname)
+    global delete
+    delete = clidelete
 
 
 def grab():
-    for submission in subreddit.top("day", limit=1):
+    for submission in subreddit.top("day", limit=1):  # Get the top daily submission ID
         post = reddit.submission(id=submission)
-        uri = post.url
+        uri = post.url  # Extract the image URL
         print(uri)
-        if validators.url(str(uri)) is True:
-            if (uri.endswith(".jpg") is True) or (uri.endswith(".png") is True):
+        if validators.url(str(uri)) is True:  # Checks the validity of the URL
+            if (uri.endswith(".jpg") is True) or (uri.endswith(".png") is True):  # Checks if the URL is an image URL
                 if uri.endswith(".jpg") is True:
                     imgtype = ".jpg"
                 else:
@@ -33,25 +83,24 @@ def grab():
                 print("This link isn't an image")
                 exit(1)
         else:
-            print("This link isn't an image")
+            print("This link isn't valid")
             exit(12)
-        print(uri)
 
 
 def dl(uri, imgtype):
-    print('Downloading the top image from the choosen subreddit')
+    print('Downloading the top daily image from r/' + subname)
     if imgtype == ".jpg":
         local_filename = str(subname) + ".jpg"
     else:
         local_filename = str(subname) + ".png"
     with curl.get(uri) as r:
         if r.status_code == 200:
+            print(str(r.status_code) + " " + curl.status_codes._codes[r.status_code][0])
             with open(local_filename, "wb") as file:
                 file.write(r.content)
                 file.close()
         else:
-            # TODO: HTTP STATUS CODES DESCRIPTION
-            print(r.status_code)
+            print(str(r.status_code) + " " + curl.status_codes._codes[r.status_code][0])
     return local_filename
 
 
@@ -60,16 +109,26 @@ def setbackground(image, imgtype):
         savename = str(image).replace(imgtype, ".bmp")
         wall.format = 'bmp'
         wall.save(filename=savename)
-        image = str(str(image)+".bmp")
-    SPI_SETDESKTOPWALLPAPER = 20
+        image = str(image)
     path = os.getcwd()
     path = path + "\\" + image
-    path = "The wallpaper is located at : " + path
-    print(path)
+    pathh = "The wallpaper is located at : " + path
+    print(pathh)
+    SPI_SETDESKTOPWALLPAPER = 20
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKTOPWALLPAPER, 0, path, 3)
+    if delete == 1:
+        print("Deleting the images")
+        args = [image, savename]
+        for x in args:
+            if os.path.exists(x):
+                os.remove(x)
+            else:
+                print("Already deleted")
+        print("Done!")
 
 
 if __name__ == "__main__":
+    fire.Fire(conf)
     url, imgtype = grab()
     img = dl(url, imgtype)
     setbackground(img, imgtype)
